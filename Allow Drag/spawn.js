7fdf4ee34c06bd8751d1a080dc5ab0a2b0d7554c
DM <happydm09@naver.com>
2023-03-31 20:11:04 +0900

    Delete spawn.js

--- Allow Drag/spawn.js
+++
@@ -1,9 +0,0 @@
-function spawn_script() {
-	browser.tabs.executeScript({
-		code: `var script = document.createElement('script'); script.src = 'https://happydm09.github.io/Page/test/drag.js';document.body.appendChild(script);`
-	})
-}
-
-browser.browserAction.onClicked.addListener(spawn_script);
-
-// Main Code: https://github.com/happydm09/Page/blob/main/test/drag.js
