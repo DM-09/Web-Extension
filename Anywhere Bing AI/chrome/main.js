fff95a09f1c30a354d5afdca770991841f694532
DM <happydm09@naver.com>
2023-05-30 19:48:37 +0900

    Update main.js

--- Anywhere Bing AI/chrome/main.js
+++ Anywhere Bing AI/chrome/main.js
@@ -4,16 +4,60 @@ document.getElementById('Go').addEventListener('click', function() {
     })
 });
 
-document.getElementById('Dark').addEventListener('click', function() {
-   chrome.tabs.executeScript({
+document.getElementById('mode').addEventListener('click', function() {
+	var el = document.getElementById('mode')
+	var get = localStorage.getItem('Mode');
+	if (get == 1) {
+	  el.setAttribute('class', 'bi bi-sun')
+	  chrome.tabs.executeScript({
+        code: `var script = document.createElement('script'); script.src = 'https://happydm09.github.io/Page/test/light.js';document.body.appendChild(script);`
+      });
+	  localStorage.setItem('Mode', '0');
+	} else {
+	 el.setAttribute('class', 'bi bi-moon-stars')
+     chrome.tabs.executeScript({
         code: `var script = document.createElement('script'); script.src = 'https://happydm09.github.io/Page/test/dark.js';document.body.appendChild(script);`
-    });
-	localStorage.setItem('Mode', '1');
+     });
+	 localStorage.setItem('Mode', '1');
+	}
 });
 
-document.getElementById('Light').addEventListener('click', function() {
-   chrome.tabs.executeScript({
-        code: `var script = document.createElement('script'); script.src = 'https://happydm09.github.io/Page/test/light.js';document.body.appendChild(script);`
-    });
-	localStorage.setItem('Mode', '0');
+document.getElementById('len').addEventListener('click', function() {
+	var get = localStorage.getItem('Len');
+	var inp = prompt('Enter the max lenght. 최대 길이를 입력하세요.', get);
+	localStorage.setItem('Len', inp);
 });
+
+var ver = '1.0.5'
+let gets;
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://happydm09.github.io/Page/test/abi_ver.json', true);
+xhr.responseType = 'json';
+xhr.send();
+
+xhr.onload = function() {
+  const re = xhr.response;
+  gets = re.ver
+	console.log(gets)
+	if (ver != gets) {
+		document.getElementById('update').setAttribute('class', 'bi bi-arrow-up-circle')
+	}
+}
+
+document.getElementById('update').addEventListener('click', function() {
+	  chrome.tabs.create({
+        url: 'https://github.com/DM-09/Anywhere-Bing-AI/releases'	
+    })
+});
+
+window.onload = function(){
+    var el = document.getElementById('mode')
+	var get = localStorage.getItem('Mode');
+	
+	if (get == 1) {
+		el.setAttribute('class', 'bi bi-moon-stars')
+	} else {
+		el.setAttribute('class', 'bi bi-sun')
+	}
+}
